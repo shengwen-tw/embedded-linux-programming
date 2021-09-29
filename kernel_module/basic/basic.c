c74706e4041387fa488fb4660f77dc1e4908da3e
Shengwen Cheng <c.shengwen.tw@gmail.com>
2021-09-29 20:58:49 +0800

    create basic kernel module example

---
+++ kernel_module/basic/basic.c
@@ -0,0 +1,19 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+static int __init hello_init(void)
+{
+	pr_info("hello world\n");
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	pr_info("goodbye world\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("MIT");
