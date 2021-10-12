a6e55dbab156b9e66e04d716b9f56eaa819cab6a
Shengwen Cheng <c.shengwen.tw@gmail.com>
2021-10-12 15:50:15 +0800

    add real-time application example

---
+++ rt-app/main.c
@@ -0,0 +1,44 @@
+#include <sys/mman.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <time.h>
+
+/* accurate delay function for real-time application */
+void delay_ms(int delay_ms)
+{
+	struct timespec ts;	
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+
+	ts.tv_nsec += delay_ms * 10e6;
+	if(ts.tv_nsec >= 10e9) {
+		ts.tv_nsec -= 10e9;
+		ts.tv_sec++;
+	}
+
+	/* use CLOCK_MONOTONIC and TIMER_ABSTIME for accurate timing */
+	clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &ts, NULL);
+}
+
+int main(void)
+{
+	/* change process priority */
+	struct sched_param sp;
+	sp.sched_priority = 30;
+
+	if(pthread_setschedparam(pthread_self(), SCHED_FIFO, &sp)) {
+		printf("Failed to set priority of the proceess.\n");
+	}
+
+	/* lock the memory page of the real-time application to prevent memory swapping */
+	if(mlockall(MCL_FUTURE | MCL_CURRENT)) {
+		printf("Failed to lock the memory, try to execure the program with root privilege.\n");
+	}
+
+	/* your real-time program's main loop */
+	while(1) {
+		printf("Real-time application is running...\n");
+		delay_ms(1000);
+	}
+
+	return 0;
+}
